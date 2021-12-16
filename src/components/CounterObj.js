0c4fcc4e7aa2f75ef7b1c8eebb96d5755e98a870
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-16 17:27:45 +0530

    feat added

--- src/components/CounterObj.js
+++ src/components/CounterObj.js
@@ -2,14 +2,19 @@ import React, { useReducer } from "react";
 
 const initialState = {
   firstCounter: 0,
+  secondCounter: 10,
 };
 
 const counterHandler = (state, action) => {
   switch (action.type) {
     case "increment":
-      return { firstCounter: state.firstCounter + action.value };
+      return { ...state, firstCounter: state.firstCounter + action.value };
     case "decrement":
-      return { firstCounter: state.firstCounter - action.value };
+      return { ...state, firstCounter: state.firstCounter - action.value };
+    case "increment2":
+      return { ...state, secondCounter: state.secondCounter + action.value };
+    case "decrement2":
+      return { ...state, secondCounter: state.secondCounter - action.value };
     case "reset":
       return initialState;
     default:
@@ -22,7 +27,8 @@ function CounterObj() {
 
   return (
     <div>
-      <h1> Count : {count.firstCounter}</h1>
+      <h1>First Count : {count.firstCounter}</h1>
+      <h1>Second Count : {count.secondCounter} </h1>
       <button onClick={() => dispatch({ type: "increment", value: 1 })}>
         Increase
       </button>
@@ -35,6 +41,21 @@ function CounterObj() {
       <button onClick={() => dispatch({ type: "decrement", value: 5 })}>
         Decrease 5
       </button>
+
+      <div>
+        <button onClick={() => dispatch({ type: "increment2", value: 1 })}>
+          Increase count(2)
+        </button>
+        <button
+          onClick={() => {
+            console.log("hi");
+            dispatch({ type: "decrement2", value: 1 });
+          }}
+        >
+          Decrease count(2)
+        </button>
+      </div>
+
       <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
